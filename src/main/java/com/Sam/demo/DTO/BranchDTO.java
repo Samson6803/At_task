7020dbc7268faaea619a3e48e602b2f1f85a0a32
Dawid <dawid.biz@outlook.com>
2023-06-05 09:27:36 +0200

    DTO classes changed to records

--- src/main/java/com/Sam/demo/DTO/BranchDTO.java
+++ src/main/java/com/Sam/demo/DTO/BranchDTO.java
@@ -1,19 +1,4 @@
 package com.Sam.demo.DTO;
 
-public class BranchDTO {
-    String name;
-    String sha;
-
-    public BranchDTO(String name, String sha) {
-        this.name = name;
-        this.sha = sha;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSha() {
-        return sha;
-    }
+public record BranchDTO(String name, String sha) {
 }
--- src/main/java/com/Sam/demo/Services/GitRepoService.java
+++ src/main/java/com/Sam/demo/Services/GitRepoService.java
@@ -1,18 +1,9 @@
 package com.Sam.demo.Services;
 
 import com.Sam.demo.DTO.RepositoryDTO;
-import com.Sam.demo.DTO.ResponseDTO;
 import com.Sam.demo.Repositories.GitRepoRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Service
